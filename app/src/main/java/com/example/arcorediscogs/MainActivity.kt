9a5febad804cdfdf9722240709e574c63e0b0f06
BuildTools <unconfigured@null.spigotmc.org>
2021-02-10 14:11:59 +0200

    final push for the day

--- app/src/main/java/com/example/arcorediscogs/MainActivity.kt
+++ app/src/main/java/com/example/arcorediscogs/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.arcorediscogs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomappbar.BottomAppBar
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val bottomAppBar = findViewById<BottomAppBar>(R.id.bottomAppBar)
+
+        bottomAppBar.setNavigationOnClickListener {
+            // Handle navigation icon press
+        }
+
+        bottomAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.search -> {
+                    // Handle search icon press
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 }
