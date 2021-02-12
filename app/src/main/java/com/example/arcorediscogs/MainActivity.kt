2a42ea491ed41d377fbf3dbdf18717a33f3475a0
teemusalo1 <teemu.matias.salo@gmail.com>
2021-02-12 13:22:03 +0200

    Image tracking fragment

--- app/src/main/java/com/example/arcorediscogs/MainActivity.kt
+++ app/src/main/java/com/example/arcorediscogs/MainActivity.kt
@@ -12,14 +12,19 @@ import com.google.android.material.bottomappbar.BottomAppBar
 
 class MainActivity : AppCompatActivity() {
     lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val bla = findViewById<BottomAppBar>(R.id.bottomAppBar)
+        val imgTrackingFragment = ImgTrackingFragment()
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment2, imgTrackingFragment)
+            commit()
+        }
 
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         viewModel.totalHits.observe(this, Observer {
-            Log.d("FYI", it.items.toString())
+            Log.d("FYI", it.pagination.items.toString())
             // viewModel.hitcountquery(name = it.artist.toString())
         })
     }
