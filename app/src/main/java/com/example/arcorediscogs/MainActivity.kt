487b2cb489ae344b92ce643fd7205f01cc338f66
BuildTools <unconfigured@null.spigotmc.org>
2021-02-15 12:13:08 +0200

    Monday Commit

--- app/src/main/java/com/example/arcorediscogs/MainActivity.kt
+++ app/src/main/java/com/example/arcorediscogs/MainActivity.kt
@@ -4,46 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.ar.core.TrackingState
+import com.google.ar.sceneform.rendering.ViewRenderable
+import com.google.ar.sceneform.ux.ArFragment
 
+var viewRenderable: ViewRenderable? = null
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val bla = findViewById<BottomAppBar>(R.id.bottomAppBar)
 
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        viewModel.totalHits.observe(this, Observer {
-            Log.d("FYI", it.items.toString())
-            // viewModel.hitcountquery(name = it.artist.toString())
-        })
-    }
-    fun search(view: View){
-        Log.d("FYI", "toimiiko")
-        viewModel.hitcountquery("rock")
+        val mainFrag = mainFrag()
+        val imageFrag = ImgTrackingFragment()
 
-
-
-        val bottomAppBar = findViewById<BottomAppBar>(R.id.bottomAppBar)
-
-        bottomAppBar.setNavigationOnClickListener {
-            // Handle navigation icon press
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment, mainFrag)
+            commit()
         }
 
-        bottomAppBar.setOnMenuItemClickListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.search -> {
-                    // Handle search icon press
-                    true
-                }
 
-                else -> false
-            }
-        }
     }
+
+
 }
