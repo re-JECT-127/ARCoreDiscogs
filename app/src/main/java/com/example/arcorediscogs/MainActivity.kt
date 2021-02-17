b33b54cfaa42b4bf78dceb75cc4d54cc43f3e930
BuildTools <unconfigured@null.spigotmc.org>
2021-02-17 11:40:23 +0200

    MORE FRAGMENTS (They don't work)

--- app/src/main/java/com/example/arcorediscogs/MainActivity.kt
+++ app/src/main/java/com/example/arcorediscogs/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.View
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val imgTrackingFragment = ImageTrackingFragment()
         supportFragmentManager.beginTransaction().apply {
-            replace(R.id.fragment2, imgTrackingFragment)
+            replace(R.id.fragment2, mainFrag())
             commit()
         }
 
@@ -26,28 +27,39 @@ class MainActivity : AppCompatActivity() {
             Log.d("FYI", it.pagination.items.toString())
             // viewModel.hitcountquery(name = it.artist.toString())
         })
-    }
-
-    fun search(view: View) {
-        Log.d("FYI", "toimiiko")
-        viewModel.hitcountquery("rock")
-
+        val fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            Log.d("mAct", "Fab clicked")
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fragment2, ImageTrackingFragment())
+                commit()
+            }
+        }
 
         val bottomAppBar = findViewById<BottomAppBar>(R.id.bottomAppBar)
 
         bottomAppBar.setNavigationOnClickListener {
+            Log.d("mAct", "Nav clicked")
             // Handle navigation icon press
         }
 
         bottomAppBar.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.search -> {
+                    Log.d("mAct", "Search clicked")
                     // Handle search icon press
                     true
+
                 }
 
                 else -> false
             }
         }
+    }
+
+    fun search(view: View) {
+        Log.d("FYI", "toimiiko")
+        viewModel.hitcountquery("rock")
+
     }
 }
--- app/src/main/java/com/example/arcorediscogs/MainFrag.kt
+++ app/src/main/java/com/example/arcorediscogs/MainFrag.kt
@@ -4,12 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.google.ar.sceneform.ux.ArFragment
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-class mainFrag : ArFragment() {
+class mainFrag : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
