af76354e4514ce9e07c18a8daa80e337dd7798c3
teemusalo1 <teemu.matias.salo@gmail.com>
2021-02-18 11:18:45 +0200

    image tracking api calls

--- app/src/main/java/com/example/arcorediscogs/ImageTrackingFragment.kt
+++ app/src/main/java/com/example/arcorediscogs/ImageTrackingFragment.kt
@@ -1,10 +1,8 @@
 package com.example.arcorediscogs
 
-import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
