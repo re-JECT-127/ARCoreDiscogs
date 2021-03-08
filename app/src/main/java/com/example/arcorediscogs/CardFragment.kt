d000905dc4e267c8ce71d981f25e10e1fa91adfa
teemusalo1 <teemu.matias.salo@gmail.com>
2021-03-08 14:31:14 +0200

    cardfragment recyclerview

--- app/src/main/java/com/example/arcorediscogs/CardFragment.kt
+++ app/src/main/java/com/example/arcorediscogs/CardFragment.kt
@@ -6,16 +6,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
-class CardFragment : Fragment(R.layout.fragment_card) {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+class CardFragment : Fragment() {
+    private val tracks: List<Result>? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v: View = inflater.inflate(R.layout.fragment_card, container, false)
 
+        val album = view?.findViewById<TextView>(R.id.textView5)
+
+        album?.text = tracks?.get(0)?.album
+        return v
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+            val rv_results = view?.findViewById<RecyclerView>(R.id.rv_results)
+        rv_results.layoutManager = LinearLayoutManager(requireContext())
+        rv_results.adapter = SearchResultAdapter()
+        }
     }
+
 
