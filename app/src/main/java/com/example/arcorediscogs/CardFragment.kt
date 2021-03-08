8738f4dd48d3d10582a5daa1f9edef9ca9f89408
teemusalo1 <teemu.matias.salo@gmail.com>
2021-03-08 13:24:49 +0200

    cardfragment recyclerview

--- app/src/main/java/com/example/arcorediscogs/CardFragment.kt
+++ app/src/main/java/com/example/arcorediscogs/CardFragment.kt
@@ -6,27 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 
-class CardFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val v: View = inflater.inflate(R.layout.item_view, container, false)
+class CardFragment : Fragment(R.layout.fragment_card) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-        val db by lazy { ResultDB.get(v.context) }
-
-        return v
     }
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-    }}
+    }
 
